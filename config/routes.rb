e07e56dfb80e1020f3d11f533ff2f0eeb4678366
Kevin Weaver <kevinweaver2@gmail.com>
2016-09-22 22:42:43 -0400

    finished twitter oath.  next, throwing on heroku and testing twitter sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  devise_scope :user do
+    get '/users/auth/:provider/upgrade' => 'omniauth_callbacks#upgrade', as: :user_omniauth_upgrade
+    get '/users/auth/:provider/setup', :to => 'omniauth_callbacks#setup'
+  end
 
-  devise_for :users
+  devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
