efdbc86fb19efaddc8c632c8ff78b548b0f4d772
Kevin Weaver <kevinweaver2@gmail.com>
2016-09-23 11:46:42 -0400

    working on tests

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ end
 
 group :test do
 	gem 'capybara'
+	gem 'factory_girl'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    #it "returns http success" do
+    #  get :index
+    #  expect(response).to have_http_status(:success)
+    #end
+
+    it "redirects to sign in if signed out" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+
 
 end
